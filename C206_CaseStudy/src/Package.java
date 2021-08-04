825b9eb0a849388d40e2f77e4ded7796e275d493
Tristen <Tristen>
2021-08-04 15:34:12 +0800

    Package Class

---
+++ C206_CaseStudy/src/Package.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20048254 Tristen Nyam, Aug 4, 2021 3:10:01 PM
+ */
+
+public class Package {
+
+}
