cc1e7bf70ff89bd84194746d5037ea404457fef5
ruiixiiang <ruiixiiang@LAPTOP-R3L48A57>
2021-08-04 15:46:13 +0800

    Quote package -Rx

---
+++ C206_CaseStudy/src/Quote.java
@@ -0,0 +1,15 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * ruiixiiang, 4 Aug 2021 3:27:01 pm
+ */
+
+/**
+ * @author ruiixiiang
+ *
+ */
+public class Quote {
+
+}
