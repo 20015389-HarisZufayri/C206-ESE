158391f51ea18365f9df9b690b30ff3ff2594f7a
Tristen Nyam <Tristen Nyam@DESKTOP-7ADN5CN>
2021-08-04 15:36:28 +0800

    Package class

--- C206_CaseStudy/src/Package.java
+++ C206_CaseStudy/src/Package.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 
 /**
  * I declare that this code was written by me.
@@ -8,5 +9,60 @@
  */
 
 public class Package {
-
+	private String packagecode;
+	private String packageDesc;
+	private LocalDateTime packageStart;
+	private LocalDateTime packageEnd;
+	private int packageAmt;
+	/**
+	 * @param packagecode
+	 * @param packageDesc
+	 * @param packageStart
+	 * @param packageEnd
+	 * @param packageAmt
+	 */
+	public Package(String packagecode, String packageDesc, LocalDateTime packageStart, LocalDateTime packageEnd,
+			int packageAmt) {
+		this.packagecode = packagecode;
+		this.packageDesc = packageDesc;
+		this.packageStart = packageStart;
+		this.packageEnd = packageEnd;
+		this.packageAmt = packageAmt;
+	}
+	/**
+	 * @return the packagecode
+	 */
+	public String getPackagecode() {
+		return packagecode;
+	}
+	/**
+	 * @return the packageDesc
+	 */
+	public String getPackageDesc() {
+		return packageDesc;
+	}
+	/**
+	 * @return the packageStart
+	 */
+	public LocalDateTime getPackageStart() {
+		return packageStart;
+	}
+	/**
+	 * @return the packageEnd
+	 */
+	public LocalDateTime getPackageEnd() {
+		return packageEnd;
+	}
+	/**
+	 * @return the packageAmt
+	 */
+	public int getPackageAmt() {
+		return packageAmt;
+	}
+	public String doStringPackage() {
+		return String.format("%-10s %-10s %-10d %-10s %s\n", getPackagecode(), getPackageDesc(), getPackageStart(),
+		getPackageEnd(), getPackageAmt());
+	}
+	
+	
 }
