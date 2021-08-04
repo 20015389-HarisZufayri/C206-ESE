ff2ce5d7a2f7e7f181bd648accc61f163d1646c0
20026101-Toh-RuiXiang <87171994+20026101-Toh-RuiXiang@users.noreply.github.com>
2021-08-04 23:58:25 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -56,22 +56,6 @@ public class C206_CaseStudy {
 		}
 	}
 
-	public static void menu() {
-		C206_CaseStudy.setHeader("RENOVATION ACE");
-		System.out.println("1. Manage User");
-		System.out.println("2. Manage Package");
-		System.out.println("3. Manage Request for Quotation");
-		System.out.println("4. Manage Quotation");
-		System.out.println("5. Manage Appointment");
-		System.out.println("6. Logout");
-		Helper.line(80, "-");
-	}
-
-	public static void setHeader(String header) {
-		Helper.line(80, "-");
-		System.out.println(header);
-		Helper.line(80, "-");
-	}
 	
 	public static void menu() {
 		C206_CaseStudy.setHeader("RENOVATION ACE");
